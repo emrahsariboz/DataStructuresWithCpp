fb5b805dbd295186ffb8db2d420ec5f2ddf7fc9a
Emrah <emrahsarboz@gmail.com>
2019-11-21 23:43:57 -0700

    Bellman Ford

--- 100DaysOfCode/Bellman-Ford/main.cpp
+++ 100DaysOfCode/Bellman-Ford/main.cpp
@@ -72,9 +72,9 @@ void BellmanFord(struct Graph *graph, int src){
 int main(int argc, char *argv[])
 {
 
-    int V = 3;
+    int V = 5;
 
-    int E = 3;
+    int E = 8;
 
     Graph *graph = createGraph(V,E);
 
@@ -86,10 +86,29 @@ int main(int argc, char *argv[])
     graph->edge[1].destination = 2;
     graph->edge[1].weight = 4;
 
-    graph->edge[2].source = 2;
-    graph->edge[2].destination = 0;
-    graph->edge[2].weight = -5;
+    graph->edge[2].source = 1;
+    graph->edge[2].destination = 2;
+    graph->edge[2].weight = 3;
 
+    graph->edge[3].source = 1;
+    graph->edge[3].destination = 3;
+    graph->edge[3].weight = 2;
+
+    graph->edge[4].source = 1;
+    graph->edge[4].destination = 4;
+    graph->edge[4].weight = 2;
+
+    graph->edge[5].source = 3;
+    graph->edge[5].destination = 2;
+    graph->edge[5].weight = 5;
+
+    graph->edge[6].source = 3;
+    graph->edge[6].destination = 1;
+    graph->edge[6].weight = 1;
+
+    graph->edge[7].source = 4;
+    graph->edge[7].destination = 3;
+    graph->edge[7].weight = -3;
 
 
 
